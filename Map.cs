0ae445ed9fed292d65fc94d630329e8517fa8e6b
WillCS <wcstibbards@gmail.com>
2019-07-07 21:30:15 +1000

    branched from Vulkan test after I couldn't fix the segfault, started work on some mapping tools.

---
+++ Map.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace Game {
+    public class Floor {
+
+        private int xSize;
+        private int ySize;
+        private int[,] floorTiles;
+
+        private Dictionary<int, string> tileMappings;
+
+        protected Floor() {
+
+        }
+
+        public int XSize { 
+            get => this.xSize;
+        }
+
+        public int YSize {
+            get => this.ySize;
+        }
+
+        public bool CoordIsInRange(int x, int y) =>
+            0 <= x && x <= this.XSize && 0 <= y && y <= this.YSize;
+
+        public string TileAt(int x, int y) {
+            if(!this.CoordIsInRange(x, y)) {
+                throw new System.Exception("Coordinate out of range.");
+            }
+
+            int tileID = floorTiles[x, y];
+            
+            if(!this.tileMappings.ContainsKey(tileID)) {
+                throw new System.Exception("Tile ID not found.");
+            }
+
+            return this.tileMappings[tileID];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using GLFW;
+
+namespace Game {
+    public class Program {
+
+        public static int WIDTH = 400;
+        public static int HEIGHT = 400;
+        
+        private static int TICK_RATE = 60;
+        private static double TICK_TIME = 1.0 / TICK_RATE;
+
+        private Window window;
+        private double timeLastLoop = 0;
+        private double accumulator = 0;
+
+        static void Main(string[] args) {
+            Program program = new Program();
+            program.Run();
+        }
+
+        private void Run() {
+            this.InitWindow();
+            this.InitOpenGL();
+            this.MainLoop();
+            this.Cleanup();
+        }
+
+        private void InitWindow() {
+            ErrorCallback errorHandler = (ErrorCode ErrorCode, IntPtr message) => {
+                Console.WriteLine(ErrorCode);
+                string errorMsg = System.Runtime.InteropServices.Marshal.PtrToStringUTF8(message);
+                Console.WriteLine(errorMsg);
+            };
+
+            Glfw.SetErrorCallback(errorHandler);
+
+            Glfw.Init();
+            this.timeLastLoop = Glfw.Time;
+
+            Glfw.WindowHint(Hint.ClientApi, GLFW.ClientApi.OpenGL);
+            Glfw.WindowHint(Hint.Resizable, GLFW.Constants.False);
+
+            this.window = Glfw.CreateWindow(WIDTH, HEIGHT, "OpenGL", Monitor.None, Window.None);
+        }
+
+        private void InitOpenGL() {
+
+        }
+
+        private void MainLoop() {
+            while(!Glfw.WindowShouldClose(this.window)) {
+                double currentTime = Glfw.Time;
+                double deltaTime = currentTime - this.timeLastLoop;
+                this.accumulator += deltaTime;
+                this.timeLastLoop = currentTime;
+
+                if(this.accumulator >= TICK_TIME * 4) {
+                    this.accumulator = TICK_TIME * 4;
+                }
+
+                while(this.accumulator >= TICK_TIME) {
+                    this.accumulator -= TICK_TIME;
+                }
+
+                Glfw.PollEvents();
+            }
+        }
+
+        private void Cleanup() {
+            Glfw.DestroyWindow(this.window);
+            Glfw.Terminate();
+        }
+    }
+}
